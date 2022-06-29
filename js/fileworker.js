786f51c5b5c1f8d61d25e10a5cd272c3108dff12
Yukimi Kazari <dev@9854.moe>
2022-06-29 17:44:03 -0400

    Fixed parent names not matching template

--- js/fileworker.js
+++ js/fileworker.js
@@ -527,7 +527,7 @@ docutrol@acr.moe - 301-399-3671 - docs.acr.moe/docutrol
 					});
 					datawatcher1.on('add', function (filePath) {
 						if (!(filePath.includes('HOLD-') || filePath.includes('PREVIEW-') || filePath.includes('FILEATT-'))) {
-							onboardFileAdd(slash("./" + filePath), "1")
+							onboardFileAdd(slash(filePath), "1")
 						}
 					})
 						.on('error', function (error) {
