d672ad582bb097b30b3c51d89b224ba63f069a88
Yukimi Kazari <dev@9854.moe>
2022-07-02 23:15:55 -0400

    Working on adding commands to adjust database properties

--- js/discord.js
+++ js/discord.js
@@ -2746,7 +2746,7 @@ This code is publicly released and is restricted by its project license
                                     case 'channel':
                                         switch (args[2].toLowerCase()) {
                                             case 'list':
-                                                const list = await db.query(`SELECT channelid, serverid, parent, virtual_cid, name, nice_name, nice_title, classification, uri, watch_folder, notify, role, role_write, role_manage, description, nsfw FROM kanmi_channels WHERE classification IS NOT NULL OR classification != 'system'`);
+                                                const list = await db.query(`SELECT channelid, serverid, parent, virtual_cid, name, nice_name, nice_title, classification, uri, watch_folder, notify, role, role_write, role_manage, description, nsfw FROM kanmi_channels WHERE classification IS NOT NULL AND classification != 'system' AND classification != 'timeline'`);
                                                 try {
                                                     await discordClient.createMessage(msg.channel.id, `Channel Metadata from Database`, [{
                                                         file: Buffer.from(JSON.stringify(list.rows, null, '\t')),
