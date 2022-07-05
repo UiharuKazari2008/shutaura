ce80922155ce2503a27db9ba910eb4110305d71f
Yukimi Kazari <dev@9854.moe>
2022-07-05 19:18:41 -0400

    Testing errors with cache preview generation

--- js/utils/generateVideoGIFThumbs.js
+++ js/utils/generateVideoGIFThumbs.js
@@ -105,7 +105,7 @@
     const mqClient = require('./mqClient')(facilityName, systemglobal);
 
 
-    setTimeout(() => {
+    setTimeout(async () => {
         const results = await db.query(`SELECT id, real_filename FROM kanmi_records WHERE (real_filename LIKE '%.mov' OR real_filename LIKE '%.mp4' OR real_filename LIKE '%.avi' OR real_filename LIKE '%.mkv' OR real_filename LIKE '%.ts' ) AND (cache_proxy IS NULL OR cache_proxy NOT LIKE '%.gif') AND filecached = 1 ORDER BY eid DESC`)
         console.log(`There are ${results.rows.length} thumbnails to be regenerated`)
         results.rows.map(message => {
