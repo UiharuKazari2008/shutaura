97a06ba9000391bab2f0f32ae815ee371d5ec0a9
Yukimi Kazari <dev@9854.moe>
2022-07-19 21:23:04 -0400

    Supports Caching

--- js/discord.js
+++ js/discord.js
@@ -5316,6 +5316,7 @@ This code is publicly released and is restricted by its project license
             "value": _ioT.join('\n').substring(0,1024)
         })
         if (activeProccessing.length > 10) { systemWarning = true; }
+        console.log(fileTicker)
         fileTicker = fileTicker.filter(e => !e.date || (e.date && (Date.now() - e.date) <= 1800000)).slice(5)
         if (fileTicker.length > 0) {
             embed.fields.push({
