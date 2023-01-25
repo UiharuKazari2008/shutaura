5c2ae77c003267124f63a01db77d6b523a07bfde
Yukimi Kazari <dev@9854.moe>
2023-01-24 23:04:56 -0500

    Updates to cross-instance config exchange

--- js/authware.js
+++ js/authware.js
@@ -1356,7 +1356,6 @@ docutrol@acr.moe - 301-399-3671 - docs.acr.moe/docutrol
                     error: 'Invalid Request'
                 })
             }
-            res.status(200).send("OK");
         });
     }
     async function refreshRemoteExchanges() {
