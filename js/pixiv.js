febea39a46af14ad085b5aa367022bf339e3c286
Yukimi Kazari <nomail@internet.net>
2023-06-18 12:36:44 -0400

    Remove token expired alert, this is not really a thing that happens and only leads to nusance errors

--- js/pixiv.js
+++ js/pixiv.js
@@ -266,7 +266,6 @@ docutrol@acr.moe - 301-399-3671 - docs.acr.moe/docutrol
                 })
                 .catch((err) => {
                     Logger.printLine("TokenSystem", "Failed to refresh token for pixiv!", "emergency", err);
-                    mqClient.sendMessage(`Pixiv Refresh Token is no longer valid, please generate a new token now!`, "crit", "TokenSystem");
                     console.error(err);
                     setTimeout(refreshToken, 60000)
                     auth = false;
