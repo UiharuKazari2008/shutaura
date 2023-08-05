5c3c00706e5cc08554cbf8d753aa2e8e5a35601a
Yukimi Kazari <dev@9854.moe>
2023-08-05 07:48:42 -0400

    AuthWare is now setup for fast cluster reponse mode, mow uses caches for remote exchanges in db to no longer need direct access to other exchnage, authware API is always online

--- js/authware.js
+++ js/authware.js
@@ -1098,6 +1098,7 @@ docutrol@acr.moe - 301-399-3671 - docs.acr.moe/docutrol
                 review_channels: [],
                 kongou_next_episode: {},
                 disabled_channels: (disabledChannels) ? disabledChannels.map(e => e.cid) : [],
+                position: 0,
                 time_generated: (new Date().valueOf()).toString(),
             };
 
