86816aa2cf6cdb7cb076765fb0c820dd63ae14fc
Yukimi Kazari <kazari@acr.moe>
2024-01-21 01:42:57 -0500

    Added CDN download of master files

--- js/cache.js
+++ js/cache.js
@@ -670,7 +670,7 @@ docutrol@acr.moe - 301-399-3671 - docs.acr.moe/docutrol
             }, Promise.resolve());
             requests.then(async () => {
                 Logger.printLine("BackupFile", `Download ${message.id}`, "debug")
-                if (Object.values(res).filter(f => !f).length > 0)
+                if (Object.values(res).filter(f => !f).length === 0)
                     await backupCompleted(`${message.server}/${message.channel}`, res.preview, res.full, res.extended_preview, res.mfull);
                 cb(requested_remotely || (Object.values(res).filter(f => !f).length === 0));
             });
