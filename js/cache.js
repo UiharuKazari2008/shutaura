1d33154fa17a83de6ec6cefa2e23fe7bf308fbce
Yukimi Kazari <kazari@acr.moe>
2024-01-21 16:47:01 -0500

    Updates to CDN

--- js/cache.js
+++ js/cache.js
@@ -269,6 +269,7 @@ docutrol@acr.moe - 301-399-3671 - docs.acr.moe/docutrol
                 complete(true);
                 break;
             default :
+                Logger.printLine("Inbox", `Failed to parse intent: ${JSON.stringify(message)}`, "err");
                 complete(true);
                 break;
         }
