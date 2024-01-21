a1928da46ff482973b5ff93734121fc59a3f606a
Yukimi Kazari <kazari@acr.moe>
2024-01-21 17:26:50 -0500

    Updates to CDN

--- js/clean-cache.js
+++ js/clean-cache.js
@@ -124,161 +124,157 @@ docutrol@acr.moe - 301-399-3671 - docs.acr.moe/docutrol
 
                     console.log(`${c.channelid} : Preview = ${previews.length} | Full = ${full.length} | Master = ${master.length}`)
 
-                    if (full.length > 0 || master.length > 0 || previews.length > 0) {
-                        let deleteID = new Map();
-                        const messages = await db.query(`SELECT x.eid, y.*
-                                                 FROM (SELECT eid, source, server, channel, attachment_name, fileid, attachment_hash, attachment_extra FROM kanmi_records WHERE source = 0 AND ((attachment_hash IS NOT NULL AND attachment_extra IS NULL) OR fileid IS NOT NULL) AND channel = ?) x
-                                                          LEFT JOIN (SELECT * FROM kanmi_records_cdn WHERE host = ?) y ON (x.eid = y.eid)`, [c.channelid, systemglobal.CDN_ID]);
-                        if (messages.rows.length > 0) {
-                            const preview_files = messages.rows.filter(e => !!e.preview_hint).map(e => e.preview_hint);
-                            const master_files = messages.rows.filter(e => !!e.mfull_hint).map(e => e.mfull_hint);
-                            const full_files = messages.rows.filter(e => !!e.full_hint).map(e => e.full_hint);
-                            const ext_preview_files = messages.rows.filter(e => !!e.ext_0_hint).map(e => e.ext_0_hint);
+                    let deleteID = new Map();
+                    const messages = await db.query(`SELECT x.eid, y.*
+                                             FROM (SELECT eid, source, server, channel, attachment_name, fileid, attachment_hash, attachment_extra FROM kanmi_records WHERE source = 0 AND ((attachment_hash IS NOT NULL AND attachment_extra IS NULL) OR fileid IS NOT NULL) AND channel = ?) x
+                                                      LEFT JOIN (SELECT * FROM kanmi_records_cdn WHERE host = ?) y ON (x.eid = y.eid)`, [c.channelid, systemglobal.CDN_ID]);
+                    if (messages.rows.length > 0) {
+                        const preview_files = messages.rows.filter(e => !!e.preview_hint).map(e => e.preview_hint);
+                        const master_files = messages.rows.filter(e => !!e.mfull_hint).map(e => e.mfull_hint);
+                        const full_files = messages.rows.filter(e => !!e.full_hint).map(e => e.full_hint);
+                        const ext_preview_files = messages.rows.filter(e => !!e.ext_0_hint).map(e => e.ext_0_hint);
 
-                            if (messages.rows.length > 100000)
-                                console.log(`Processing Orphaned Files - Preview`)
-                            await new Promise(orphok => {
-                                let removed = 0;
-                                for (let i = 0; i < previews.length; i++) {
-                                    if (preview_files.indexOf(previews[i]) === -1) {
-                                        try {
-                                            fs.unlinkSync(path.join(dir_previews, previews[i]))
-                                            removed++;
-                                        } catch (e) {
+                        if (messages.rows.length > 100000)
+                            console.log(`Processing Orphaned Files - Preview`)
+                        await new Promise(orphok => {
+                            let removed = 0;
+                            for (let i = 0; i < previews.length; i++) {
+                                if (preview_files.indexOf(previews[i]) === -1) {
+                                    try {
+                                        fs.unlinkSync(path.join(dir_previews, previews[i]))
+                                        removed++;
+                                    } catch (e) {
 
-                                        }
                                     }
-                                }
-                                if (removed > 0) {
-                                    Logger.printLine("Sweeper", `Removed ${removed} previews deleted items storage`, "info");
-                                    previews = (fs.existsSync(dir_previews)) ? fs.readdirSync(dir_previews) : [];
-                                }
-                                orphok();
-                            })
-                            if (messages.rows.length > 100000)
-                                console.log(`Processing Orphaned Files - Full`)
-                            await new Promise(orphok => {
-                                let removed = 0;
-                                for (let i = 0; i < full.length; i++) {
-                                    if (full_files.indexOf(full[i]) === -1) {
-                                        try {
-                                            fs.unlinkSync(path.join(dir_full, full[i]))
-                                            removed++;
-                                        } catch (e) {
-
-                                        }
-                                    }
-                                }
-                                if (removed > 0) {
-                                    Logger.printLine("Sweeper", `Removed ${removed} full deleted items storage`, "info");
-                                    full = (fs.existsSync(dir_full)) ? fs.readdirSync(dir_full) : [];
-                                }
-                                orphok();
-                            })
-                                console.log(`Processing Orphaned Files - Master`)
-                            await new Promise(orphok => {
-                                let removed = 0;
-                                for (let i = 0; i < master.length; i++) {
-                                    if (master_files.indexOf(master[i]) === -1) {
-                                        try {
-                                            fs.unlinkSync(path.join(dir_master, master[i]))
-                                            removed++;
-                                        } catch (e) {
-
-                                        }
-                                    }
-                                }
-                                if (removed > 0) {
-                                    Logger.printLine("Sweeper", `Removed ${removed} master deleted items storage`, "info");
-                                    master = (fs.existsSync(dir_master)) ? fs.readdirSync(dir_master) : [];
-                                }
-                                orphok();
-                            })
-                            if (messages.rows.length > 100000)
-                                console.log(`Processing Orphaned Files - Ext Previews`)
-                            await new Promise(orphok => {
-                                let removed = 0;
-                                for (let i = 0; i < ext_previews.length; i++) {
-                                    if (ext_preview_files.indexOf(ext_previews[i]) === -1) {
-                                        try {
-                                            fs.unlinkSync(path.join(dir_ext_previews, ext_previews[i]))
-                                            removed++;
-                                        } catch (e) {
-
-                                        }
-                                    }
-                                }
-                                if (removed > 0) {
-                                    Logger.printLine("Sweeper", `Removed ${removed} ext_previews deleted items storage`, "info");
-                                    ext_previews = (fs.existsSync(dir_ext_previews)) ? fs.readdirSync(dir_ext_previews) : [];
-                                }
-                                orphok();
-                            })
-
-                            if (messages.rows.length > 100000)
-                                console.log(`Processing Stored Files - Master`)
-                            for (let i = 0; i < master_files.length; i++) {
-                                if (master.indexOf(master_files[i]) === -1) {
-                                    deleteID.set(master_files[i].eid, false)
                                 }
                             }
-                            if (messages.rows.length > 100000)
-                                console.log(`Processing Stored Files - Full`)
-                            for (let i = 0; i < full_files.length; i++) {
-                                if (full.indexOf(full_files[i]) === -1) {
-                                    deleteID.set(full_files[i].eid, false)
+                            if (removed > 0) {
+                                Logger.printLine("Sweeper", `Removed ${removed} previews deleted items storage`, "info");
+                                previews = (fs.existsSync(dir_previews)) ? fs.readdirSync(dir_previews) : [];
+                            }
+                            orphok();
+                        })
+                        if (messages.rows.length > 100000)
+                            console.log(`Processing Orphaned Files - Full`)
+                        await new Promise(orphok => {
+                            let removed = 0;
+                            for (let i = 0; i < full.length; i++) {
+                                if (full_files.indexOf(full[i]) === -1) {
+                                    try {
+                                        fs.unlinkSync(path.join(dir_full, full[i]))
+                                        removed++;
+                                    } catch (e) {
+
+                                    }
                                 }
                             }
-                            if (messages.rows.length > 100000)
-                                console.log(`Processing Stored Files - Preview`)
-                            for (let i = 0; i < preview_files.length; i++) {
-                                if (previews.indexOf(preview_files[i]) === -1) {
-                                    deleteID.set(preview_files[i].eid, false)
+                            if (removed > 0) {
+                                Logger.printLine("Sweeper", `Removed ${removed} full deleted items storage`, "info");
+                                full = (fs.existsSync(dir_full)) ? fs.readdirSync(dir_full) : [];
+                            }
+                            orphok();
+                        })
+                            console.log(`Processing Orphaned Files - Master`)
+                        await new Promise(orphok => {
+                            let removed = 0;
+                            for (let i = 0; i < master.length; i++) {
+                                if (master_files.indexOf(master[i]) === -1) {
+                                    try {
+                                        fs.unlinkSync(path.join(dir_master, master[i]))
+                                        removed++;
+                                    } catch (e) {
+
+                                    }
                                 }
                             }
-                            if (messages.rows.length > 100000)
-                                console.log(`Processing Stored Files - Ext Previews`)
-                            for (let i = 0; i < ext_preview_files.length; i++) {
-                                if (ext_previews.indexOf(ext_preview_files[i]) === -1) {
-                                    deleteID.set(ext_preview_files[i].eid, false)
+                            if (removed > 0) {
+                                Logger.printLine("Sweeper", `Removed ${removed} master deleted items storage`, "info");
+                                master = (fs.existsSync(dir_master)) ? fs.readdirSync(dir_master) : [];
+                            }
+                            orphok();
+                        })
+                        if (messages.rows.length > 100000)
+                            console.log(`Processing Orphaned Files - Ext Previews`)
+                        await new Promise(orphok => {
+                            let removed = 0;
+                            for (let i = 0; i < ext_previews.length; i++) {
+                                if (ext_preview_files.indexOf(ext_previews[i]) === -1) {
+                                    try {
+                                        fs.unlinkSync(path.join(dir_ext_previews, ext_previews[i]))
+                                        removed++;
+                                    } catch (e) {
+
+                                    }
                                 }
                             }
+                            if (removed > 0) {
+                                Logger.printLine("Sweeper", `Removed ${removed} ext_previews deleted items storage`, "info");
+                                ext_previews = (fs.existsSync(dir_ext_previews)) ? fs.readdirSync(dir_ext_previews) : [];
+                            }
+                            orphok();
+                        })
 
-                            if (deleteID.size > 0) {
-                                const values = Array.from(deleteID.keys()).filter(e => !!e)
-                                if (values.length > 100) {
-                                    function splitArray(array, chunkSize) {
-                                        const result = [];
+                        if (messages.rows.length > 100000)
+                            console.log(`Processing Stored Files - Master`)
+                        for (let i = 0; i < master_files.length; i++) {
+                            if (master.indexOf(master_files[i]) === -1) {
+                                deleteID.set(master_files[i].eid, false)
+                            }
+                        }
+                        if (messages.rows.length > 100000)
+                            console.log(`Processing Stored Files - Full`)
+                        for (let i = 0; i < full_files.length; i++) {
+                            if (full.indexOf(full_files[i]) === -1) {
+                                deleteID.set(full_files[i].eid, false)
+                            }
+                        }
+                        if (messages.rows.length > 100000)
+                            console.log(`Processing Stored Files - Preview`)
+                        for (let i = 0; i < preview_files.length; i++) {
+                            if (previews.indexOf(preview_files[i]) === -1) {
+                                deleteID.set(preview_files[i].eid, false)
+                            }
+                        }
+                        if (messages.rows.length > 100000)
+                            console.log(`Processing Stored Files - Ext Previews`)
+                        for (let i = 0; i < ext_preview_files.length; i++) {
+                            if (ext_previews.indexOf(ext_preview_files[i]) === -1) {
+                                deleteID.set(ext_preview_files[i].eid, false)
+                            }
+                        }
 
-                                        for (let i = 0; i < array.length; i += chunkSize) {
-                                            result.push(array.slice(i, i + chunkSize));
-                                        }
+                        if (deleteID.size > 0) {
+                            const values = Array.from(deleteID.keys()).filter(e => !!e)
+                            if (values.length > 100) {
+                                function splitArray(array, chunkSize) {
+                                    const result = [];
 
-                                        return result;
+                                    for (let i = 0; i < array.length; i += chunkSize) {
+                                        result.push(array.slice(i, i + chunkSize));
                                     }
 
-                                    (splitArray(values, 50)).map(async h => {
-                                        await db.query(`DELETE
-                                                        FROM kanmi_records_cdn
-                                                        WHERE (${h.map(e => 'heid = ' + (parseInt(e.toString()) * parseInt(systemglobal.CDN_ID.toString()))).join(' OR ')})
-                                                          AND host = ?
-                                                        LIMIT 100`, [systemglobal.CDN_ID]);
-                                        console.log('DELETE BATCH')
-                                    })
+                                    return result;
+                                }
 
-                                } else if (values.length > 0) {
+                                (splitArray(values, 50)).map(async h => {
                                     await db.query(`DELETE
                                                     FROM kanmi_records_cdn
-                                                    WHERE (${values.map(e => 'heid = ' + (parseInt(e.toString()) * parseInt(systemglobal.CDN_ID.toString()))).join(' OR ')})
+                                                    WHERE (${h.map(e => 'heid = ' + (parseInt(e.toString()) * parseInt(systemglobal.CDN_ID.toString()))).join(' OR ')})
                                                       AND host = ?
                                                     LIMIT 100`, [systemglobal.CDN_ID]);
-                                    Logger.printLine("SQL", `Removed ${deleteID.size} Invalid items from cache`, "info");
-                                }
+                                    console.log('DELETE BATCH')
+                                })
+
+                            } else if (values.length > 0) {
+                                await db.query(`DELETE
+                                                FROM kanmi_records_cdn
+                                                WHERE (${values.map(e => 'heid = ' + (parseInt(e.toString()) * parseInt(systemglobal.CDN_ID.toString()))).join(' OR ')})
+                                                  AND host = ?
+                                                LIMIT 100`, [systemglobal.CDN_ID]);
+                                Logger.printLine("SQL", `Removed ${deleteID.size} Invalid items from cache`, "info");
                             }
-                            resolveChannel();
-                        } else {
-                            resolveChannel();
                         }
+                        resolveChannel();
                     } else {
                         resolveChannel();
                     }
