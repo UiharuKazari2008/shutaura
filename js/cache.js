d058d1a742c2c5a7f48f6c71cafe13e99ca1b10e
Yukimi Kazari <kazari@acr.moe>
2024-01-23 03:12:23 -0500

    Updates to CDN

--- js/cache.js
+++ js/cache.js
@@ -828,7 +828,7 @@ docutrol@acr.moe - 301-399-3671 - docs.acr.moe/docutrol
         })()
         const q = `SELECT x.*, y.heid, y.full, y.mfull, y.preview, y.ext_0, y.ext_1, y.ext_2, y.ext_3
                        FROM (SELECT rec.*, ext.data
-                             FROM (SELECT * FROM kanmi_records WHERE source = 0 ${included_focus} ${(ignoreQuery.length > 0) ? ' AND (' + ignoreQuery.join(' AND ') + ')' : ''}) rec
+                             FROM (SELECT * FROM kanmi_records WHERE source = 0 AND hidden = 0 ${included_focus} ${(ignoreQuery.length > 0) ? ' AND (' + ignoreQuery.join(' AND ') + ')' : ''}) rec
                                       LEFT OUTER JOIN (SELECT * FROM kanmi_records_extended) ext ON (rec.eid = ext.eid)) x
                                 LEFT OUTER JOIN (SELECT * FROM kanmi_records_cdn WHERE host = ?) y ON (x.eid = y.eid)
                        WHERE (y.heid IS NULL OR (x.fileid IS NOT NULL AND y.mfull = 0 ${(systemglobal.CDN_Ignore_Master_Channels) ? 'AND x.channel NOT IN (' + systemglobal.CDN_Ignore_Master_Channels.join(', ') + ')' : ''}))
@@ -858,7 +858,7 @@ docutrol@acr.moe - 301-399-3671 - docs.acr.moe/docutrol
                    FROM (SELECT rec.*, ext.data
                          FROM (SELECT *
                                FROM kanmi_records
-                               WHERE source = 0
+                               WHERE source = 0 AND hidden = 0 
                                  AND eid IN (SELECT eid
                                              FROM (SELECT eid, episode_num, show_id
                                                    FROM kongou_episodes
