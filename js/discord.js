38730b395c8488663a9049a727d4a2f2644f6155
Yukimi Kazari <kazari@acr.moe>
2024-03-13 22:32:43 -0400

    Update deletion process in cache.js
    The code changes improve the deletion process from the kanmi_records_cdn database. Instead of deleting items one by one, we've replaced it with a more efficient batch deletion process. This new approach collects all eids that need to be removed and deletes them together, improving performance especially when dealing with large numbers of items to be deleted.

--- js/discord.js
+++ js/discord.js
@@ -9414,6 +9414,23 @@ This code is publicly released and is restricted by its project license
                                 Logger.printLine("SBI SpannedFileRequest", `Failed to get spanned file from discord with ID "${u.messageid}" for file ${req.query.uuid}`, "error", e);
                             }
                             if (pm && pm.attachments && pm.attachments.length > 0) {
+                                (async () => {
+                                    try {
+                                        const a = pm.attachments[0].url.split('?')[1];
+                                        let ex = null;
+                                        try {
+                                            let exSearch = new URLSearchParams(a);
+                                            const _ex = Number('0x' + exSearch.get('ex'));
+                                            ex = moment.unix(_ex).format('YYYY-MM-DD HH:mm:ss');
+                                        } catch (err) {
+                                            Logger.printLine("Discord", `Failed to get auth expire time value for parity database row!`, "debug", err);
+                                        }
+                                        auth = `?${a}`;
+                                        await db.query(`UPDATE discord_multipart_files SET url = ?, auth = ?, auth_expire = ? WHERE channelid = ? AND messageid = ?`, [pm.attachments[0].url.split('/attachments').pop().split('?')[0], a, ex, u.channelid, u.messageid])
+                                    } catch (e) {
+                                        console.error(e)
+                                    }
+                                })()
                                 filelist.push(pm.attachments[0].url);
                                 partOk(true);
                             } else {
