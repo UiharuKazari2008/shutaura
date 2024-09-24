289f22b82935e9bf8f9e399cabd58aa6f0b9fe97
UiharuKazari2008 <nomail@internet.net>
2024-09-24 06:04:47 -0400

    das

--- js/webCrawer.js
+++ js/webCrawer.js
@@ -678,7 +678,7 @@ This code is publicly released and is restricted by its project license
                                 try {
                                     await Promise.all(thisArticle.attachments.map(async (image, imageIndex) => {
                                         let title = `**🎏 ${userProfile.name} (${source})** : ***${thisArticle.title}${(thisArticle.attachments.length > 1) ? " (" + (imageIndex + 1) + "/" + thisArticle.attachments.length + ")" : ""}***\n`;
-                                        if (thisArticle.content > 0) {
+                                        if (thisArticle.content && thisArticle.content.length > 0) {
                                             let text = stripHtml(thisArticle.content);
                                             if ((title.length + text.length) > 2000) {
                                                 const maxLinksLength = 2000 - (text.length - (thisArticle.real_url.length + 10));
