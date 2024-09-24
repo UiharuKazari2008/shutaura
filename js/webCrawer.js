442fe7cec918ae8c33d4cbcf2248e61c95af452e
UiharuKazari2008 <nomail@internet.net>
2024-09-24 05:10:29 -0400

    das

--- js/webCrawer.js
+++ js/webCrawer.js
@@ -650,8 +650,7 @@ This code is publicly released and is restricted by its project license
                     if (userFeed && userFeed.length > 0) {
                         let counter = 0
                         await Promise.all(userFeed.map(async (thisArticle, thisArticleIndex, articleArray) => {
-                            console.log(thisArticle.attachments);
-                            if (thisArticle.attachments && thisArticle.attachments > 0) {
+                            if (thisArticle.attachments && thisArticle.attachments.length > 0) {
                                 let backlog = false;
                                 if (counter > 3) {
                                     backlog = true
