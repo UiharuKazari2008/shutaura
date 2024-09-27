d21ade409bb02767472b1bee205237572f1cf131
Yukimi Kazari <kazari@acr.moe>
2024-09-27 14:39:47 -0400

    ˚

--- js/fileworker.js
+++ js/fileworker.js
@@ -1134,37 +1134,42 @@ docutrol@acr.moe - 301-399-3671 - docs.acr.moe/docutrol
 												fs.mkdirSync(PartsFilePath, {recursive: true})
 												let requests = cacheresponse.filter(e => e.valid === 1 && !(!e.url)).map(e => e.url).sort((x, y) => (x.split('.').pop() < y.split('.').pop()) ? -1 : (y.split('.').pop() > x.split('.').pop()) ? 1 : 0).reduce((promiseChain, URLtoGet, URLIndex) => {
 													return promiseChain.then(() => new Promise((resolve) => {
-														const DestFilename = path.join(PartsFilePath, `${URLIndex}.par`)
-														const stream = request.get({
-															url: URLtoGet,
-															headers: {
-																'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-																'accept-language': 'en-US,en;q=0.9',
-																'cache-control': 'max-age=0',
-																'sec-ch-ua': '"Chromium";v="92", " Not A;Brand";v="99", "Microsoft Edge";v="92"',
-																'sec-ch-ua-mobile': '?0',
-																'sec-fetch-dest': 'document',
-																'sec-fetch-mode': 'navigate',
-																'sec-fetch-site': 'none',
-																'sec-fetch-user': '?1',
-																'upgrade-insecure-requests': '1',
-																'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36 Edg/92.0.902.73'
-															},
-														}).pipe(fs.createWriteStream(DestFilename))
-														// Write File to Temp Filesystem
-														stream.on('finish', function () {
-															Logger.printLine("MPFDownload", `Downloaded Part #${URLIndex} : ${DestFilename}`, "debug", {
-																URL: URLtoGet,
-																DestFilename: DestFilename,
-																CompleteFilename: fileName
+														try {
+															const DestFilename = path.join(PartsFilePath, `${URLIndex}.par`)
+															const stream = request.get({
+																url: URLtoGet,
+																headers: {
+																	'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+																	'accept-language': 'en-US,en;q=0.9',
+																	'cache-control': 'max-age=0',
+																	'sec-ch-ua': '"Chromium";v="92", " Not A;Brand";v="99", "Microsoft Edge";v="92"',
+																	'sec-ch-ua-mobile': '?0',
+																	'sec-fetch-dest': 'document',
+																	'sec-fetch-mode': 'navigate',
+																	'sec-fetch-site': 'none',
+																	'sec-fetch-user': '?1',
+																	'upgrade-insecure-requests': '1',
+																	'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36 Edg/92.0.902.73'
+																},
+															}).pipe(fs.createWriteStream(DestFilename))
+															// Write File to Temp Filesystem
+															stream.on('finish', function () {
+																Logger.printLine("MPFDownload", `Downloaded Part #${URLIndex} : ${DestFilename}`, "debug", {
+																	URL: URLtoGet,
+																	DestFilename: DestFilename,
+																	CompleteFilename: fileName
+																})
+																itemsCompleted.push(DestFilename);
+																resolve()
+															});
+															stream.on("error", function (err) {
+																mqClient.sendMessage(`Part of the multipart file failed to download! ${URLtoGet}`, "err", "MPFDownload", err)
+																resolve()
 															})
-															itemsCompleted.push(DestFilename);
+														} catch (e) {
+															mqClient.sendMessage(`Part of the multipart file failed to download! ${URLtoGet}: ${e.message}`, "err", "MPFDownload", err)
 															resolve()
-														});
-														stream.on("error", function (err) {
-															mqClient.sendMessage(`Part of the multipart file failed to download! ${URLtoGet}`, "err", "MPFDownload", err)
-															resolve()
-														})
+														}
 													}))
 												}, Promise.resolve());
 												requests.then(async () => {
