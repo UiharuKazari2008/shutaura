97ad596a5cd09fbe02839044be2eea49c8ad2307
Yukimi Kazari <kazari@acr.moe>
2024-10-06 03:03:31 -0400

    ˚

--- js/cache.js
+++ js/cache.js
@@ -130,6 +130,8 @@ docutrol@acr.moe - 301-399-3671 - docs.acr.moe/docutrol
                     systemglobal.CDN_Trash_Path = _backup_config[0].param_data.trash_path;
                 if (_backup_config[0].param_data.match_latest)
                     systemglobal.CDN_Match_Latest = _backup_config[0].param_data.match_latest;
+                if (_backup_config[0].param_data.delay_download)
+                    systemglobal.CDN_Delay_Pull = _backup_config[0].param_data.delay_download;
             }
             // {"backup_parts": true, "interval_min": 5, "backup_base_path": "/mnt/backup/", "pickup_base_path": "/mnt/data/kanmi-files/", "items_per_backup" : 2500}
             const _backup_ignore = systemparams_sql.filter(e => e.param_key === 'seq_cdn.ignore');
@@ -1249,13 +1251,15 @@ docutrol@acr.moe - 301-399-3671 - docs.acr.moe/docutrol
             }
             return 'AND ((attachment_hash IS NOT NULL AND attachment_extra IS NULL) OR fileid IS NOT NULL)'
         })()
-        const q = `SELECT x.*, y.heid, y.full, y.mfull, y.preview, y.ext_0, y.ext_1, y.ext_2, y.ext_3, IF(x.attachment_auth_ex > NOW(), 1, 0) AS attachment_auth_valid, IF(x.cache_auth_ex > NOW(), 1, 0) AS cache_auth_valid
+        const q = `SELECT x.*, y.heid, y.full, y.mfull, y.preview, y.ext_0, y.ext_1, y.ext_2, y.ext_3, IF(x.attachment_auth_ex BETWEEN NOW() AND NOW() + INTERVAL 4 HOUR, 1, 0) AS attachment_cdn_valid, IF(x.attachment_auth_ex > NOW(), 1, 0) AS attachment_auth_valid, IF(x.cache_auth_ex > NOW(), 1, 0) AS cache_auth_valid
                        FROM (SELECT rec.*, ext.data
                              FROM (SELECT * FROM kanmi_records WHERE source = 0 AND flagged = 0 AND hidden = 0 ${included_focus} ${(ignoreQuery.length > 0) ? ' AND (' + ignoreQuery.join(' AND ') + ')' : ''}) rec
                                       LEFT OUTER JOIN (SELECT * FROM kanmi_records_extended) ext ON (rec.eid = ext.eid)) x
                                 LEFT OUTER JOIN (SELECT * FROM kanmi_records_cdn WHERE host = ?) y ON (x.eid = y.eid)
-                       WHERE (y.heid IS NULL OR (data IS NOT NULL AND y.ext_0 = 0) OR (x.fileid IS NOT NULL AND y.mfull = 0 ${(systemglobal.CDN_Ignore_Master_Channels) ? 'AND x.channel NOT IN (' + systemglobal.CDN_Ignore_Master_Channels.join(', ') + ')' : ''}))
-                         AND x.id NOT IN (SELECT id FROM kanmi_cdn_skipped)
+                       WHERE (y.heid IS NULL OR 
+                              (data IS NOT NULL AND y.ext_0 = 0) OR 
+                              (x.fileid IS NOT NULL AND y.mfull = 0 ${(systemglobal.CDN_Ignore_Master_Channels) ? 'AND x.channel NOT IN (' + systemglobal.CDN_Ignore_Master_Channels.join(', ') + ')' : ''}))
+                         AND x.id NOT IN (SELECT id FROM kanmi_cdn_skipped) ${(systemglobal.CDN_Delay_Pull) ? 'AND attachment_cdn_valid = 0' : ''}
                        ORDER BY ${(systemglobal.CDN_Match_Latest) ? "eid DESC" : "RAND()"}
                        LIMIT ?`;
         console.log(q)
