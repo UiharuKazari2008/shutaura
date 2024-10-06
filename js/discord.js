9fe0f5bccd72b6ca745ca7855a21d15e3998405a
Yukimi Kazari <kazari@acr.moe>
2024-10-06 02:06:11 -0400

    Correct issues with CDN not dropping messages

--- js/discord.js
+++ js/discord.js
@@ -8278,7 +8278,7 @@ This code is publicly released and is restricted by its project license
                                 }
                                 const eidData = (await db.query(`SELECT eid FROM kanmi_records WHERE id = ?`, [sqlObject.id])).rows
                                 // Write to CDN
-                                if (((sqlObject.fileid && !Discord_No_CDN_Reload_Spanned) || !systemglobal.Discord_No_CDN_Reload) && !noCDNReload)
+                                if (((sqlObject.fileid && !Discord_No_CDN_Reload_Spanned) || (!sqlObject.fileid && !systemglobal.Discord_No_CDN_Reload)) && !noCDNReload)
                                     mqClient.cdnRequest({ messageIntent: "Reload", messageData: { ...eidData[0] }, messageUpdate: { ...sqlObject}, reCache: true });
                                 if (chDbval.notify !== null) {
                                     try {
@@ -8622,7 +8622,7 @@ This code is publicly released and is restricted by its project license
                     SendMessage("SQL Error occurred when saving to the message cache", "err", 'main', "SQL", addedMessage.error)
                     console.error(addedMessage.error)
                 }
-                if (!systemglobal.Discord_No_CDN_Reload || refrance.reload_cdn) {
+                if (((sqlObject.fileid && !Discord_No_CDN_Reload_Spanned) || (!sqlObject.fileid && !systemglobal.Discord_No_CDN_Reload)) || refrance.reload_cdn) {
                     mqClient.cdnRequest({
                         messageIntent: "Reload",
                         messageData: {
